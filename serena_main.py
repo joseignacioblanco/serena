15c71fab0313696c30e68667ee35bc9f4c7444c6
joseignacioblanco <tombonia@gmail.com>
2024-11-12 01:01:58 -0300

    corrijo el yibang

--- serena_main.py
+++ serena_main.py
@@ -1,4 +1,5 @@
-#! /usr/bin/python3  # Este es el shebang para que se ejecute como script de python.
+#! /usr/bin/python3
+# Este es el shebang para que se ejecute como script de python.
 
 #------------------------------ PROYECTO SERENA 9/NOV/2024 --------------------------
 # AUTOR: JOSE BLANCO
