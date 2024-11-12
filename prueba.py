3eea8a9de1de375528e7f36a4c9efb0088ffff23
joseignacioblanco <tombonia@gmail.com>
2024-11-12 18:13:31 -0300

    agrego la prueba de el rreader

--- prueba.py
+++ prueba.py
@@ -1,13 +1,25 @@
-#! /usr/bin/python3  # Este es el shebang para que se ejecute como script de python.
+#! /usr/bin/python3
 
-from modulo1 import *
+# Este es el shebang para que se ejecute como script de python.
+
+'''from modulo1 import *
 
 print("mondongo")
 
 
 def main():
     print("mondongo del mein")
-    saluda("mondongo")
+    saluda("mondongo")'''
 
+
+from mfrc522 import SimpleMFRC522
+reader = SimpleMFRC522()
+try:
+    while True:
+        id, text = reader.read()
+        print(id)
+        print(text)
+finally:
+    GPIO.cleanup()
 if __name__ == "__main__":
     main()
