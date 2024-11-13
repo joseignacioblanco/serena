adc81ff250fe60d43059c7c8c2f3c86625b4ac92
joseignacioblanco <tombonia@gmail.com>
2024-11-12 22:36:41 -0300

    pelajiada y version previa estable

--- serena_main.py
+++ serena_main.py
@@ -1,75 +1,53 @@
 #! /usr/bin/python3
-# Este es el shebang para que se ejecute como script de python. no puede haber nada mas en a primera linea sino no anda.
+# Este es el shebang para que se ejecute como script de python.
+# no puede haber nada mas en a primera linea sino no anda.
+
 
 #------------------------------ PROYECTO SERENA 9/NOV/2024 --------------------------
 # AUTOR: JOSE BLANCO.
 # CONTACTO: tombonia@gmail.com
 # Descripción: Implementa un control de acceso en una Raspberry pi para un consorio.
+# Lenguaje preferido: Python.
 #------------------------------------------------------------------------------------
 
-#Evidentemente los comentarios en python se hacen con #
-
-#como e el tema para que quede como archivo ejecutable?
-# con el shebang #! /usr/bin/python3 pero esto es solo para linux.  en windors ya salen ejecutables.
-# como yo lo hago en la rrasberri, entonces va el shebang y todo
 
 # Aqui pone todas las bibliotecas de python que va a usar.
-# se usa: from <espacio de nombres> import <bibliotecas> (verificar si es asi lo correcto) para importar todo uso el operador * o sino impport modulo y despues llamo a las funciones con el operador . moduo1.saluda()
 
-
-
-
-import RPi.GPIO as GPIO # que onda el as ese: bueno parece que es para poner GPIO.setmode y no  RPi.GPIO.setmode en la linea 30 mas abajo.
-from mfrc522 import SimpleMFRC522 # maneja el modulo rfid.
-#import requests # supongo que es algo para macaniar con internet y las webs.
-#import os # manipulla archivos.
+import RPi.GPIO as GPIO # El as ese le da un alias al modulo: para poner GPIO.setmode y no  RPi.GPIO.setmode
+from mfrc522 import SimpleMFRC522 # Maneja el modulo rfid.
 import time # para manejar cosas que tienen que ver con horarios, dias, tiempo, etc
-#from implement import * #importa todas las funciones.
 
 #------------------------------------------------------------------------------------------
 
-
 # Configuración de pines GPIO y RFID
 
-#GPIO.setmode(GPIO.BOARD) #para que tome la numeracion de la placa y no de la broadcom.
-
-lector_1 = SimpleMFRC522() # no se que carajos configura de la placa modulo rfid
-
-# Configuración del relé (deberian ser dos puertas no una sola vidrio y reja)
-
-RELAY_PIN = 5 # configura el pin 18 para uno de los reles que van a abrir la puerta
-
-#GPIO.setup(RELAY_PIN, GPIO.OUT) # configura como salida.
-
-#GPIO.output(RELAY_PIN, GPIO.LOW)  # Asegura que el relé comience apagado, deberia ser normal cerrado.
-
-# Configuración del URL de Google Drive
-
-#FILE_URL = "https://drive.google.com/uc?export=download&id=1A2B3C4D5E6F7G8H9"  # Reemplaza con tu enlace de descarga (esto veremos como lo implemento.  capaz que mejor con una web o si, un drive. o ver si se puede de gitjab..)
-
-SD_FILE_PATH = "/home/pi/Documents/serena/autorized_cards.txt"
-#SD_FILE_PATH = "/home/pi/Documentos/serena/autorized_cards.txt"  # Ruta donde se almacena el archivo en la Raspberry Pi (este es el archivo donde estan los id  de las tarjetas con la info de cada usuario: ver como vincular cada id con persona. va tener que parsear el id o usar un archivo binario y no txt)
+lector_1 = SimpleMFRC522() # Crea una instancia de la clase que maneja la placa RC522.
+RELAY_PIN = 5 #Constante simbolica, despues debo ponerla en un header o algo asi..
+SD_FILE_PATH = "/home/pi/Documents/serena/autorized_cards.txt"# Ruta donde se almacena el archivo en la Raspberry Pi (este es el archivo donde estan los id  de las tarjetas con la info de cada usuario: ver como vincular cada id con persona. va tener que parsear el id o usar un archivo binario y no txt)
 
 #---------------------------------------------------------------------------------------------------------------
 
-
-#mein
-
+# Funcion principal de entrada a la aplicación
 
 def main():
     #configuracion_setup()
+    #loop()
 
     print("Sistema de Control de Acceso Iniciado.")
     while True:
         try:
-            GPIO.setmode(GPIO.BOARD)
-            GPIO.setup(RELAY_PIN, GPIO.OUT) # configura como salida.
+            GPIO.setmode(GPIO.BOARD) # Configura el modo numeracion de placa.
+            GPIO.setup(RELAY_PIN, GPIO.OUT) # configura el relé_pin como salida. esto debe ir al setup
             GPIO.output(RELAY_PIN, GPIO.LOW)  # Asegura que el relé comience apagado, deberia ser normal cerrado.
+            
+            #lee la tarjeta:
 
             print("Esperando una tarjeta...")
-            card_id, text = lector_1.read() #asignacion multiple
+            card_id, text = lector_1.read() #asignacion multiple, a text nunca lo usa.
             card_id = str(card_id).strip() #strip le saca los espacios y str lo castea a estring
-            print(f"Tarjeta leída: {card_id}")
+            print(f"Tarjeta leída: {card_id}") #print con formato.
+            
+            #se fija si esta autorizada
 
             if is_card_authorized(card_id):
                 print("Acceso permitido.")
@@ -88,6 +66,7 @@ def main():
             GPIO.cleanup() #si lo dejo puesto, me deja el gpio sin asignar hasta la proxima tarjeta
 
 
+#todo este funcionaje deberia ponerlo en distintos archivos modulos.
 
 
 def activate_relay():
@@ -100,20 +79,6 @@ def activate_relay():
     print("Relé desactivado - Cerrando puerta")
 
 
-'''def configuracion_setup():
-
-    GPIO.setmode(GPIO.BOARD) #para que tome la numeracion de la placa y no de la broadcom.
-    #lector_1 = SimpleMFRC522() # no se que carajos configura de la placa modulo rfid
-
-    # Configuración del relé (deberian ser dos puertas no una sola vidrio y reja)
-
-    RELAY_PIN = 5 # configura el pin 18 para uno de los reles que van a abrir la puerta
-    GPIO.setup(RELAY_PIN, GPIO.OUT) # configura como salida.
-
-    GPIO.output(RELAY_PIN, GPIO.LOW)
-    print("configurado")'''
-
-
 
 def is_card_authorized(card_id):
     try:
@@ -123,11 +88,6 @@ def is_card_authorized(card_id):
     except FileNotFoundError:
         print("El archivo de autorizaciones no se encuentra.")
         return False
-
-
-
-
-
 
 
 #esto es lo del tema que paiton necesita un punto de entrada por mein y no se por que se lo indica asi.
