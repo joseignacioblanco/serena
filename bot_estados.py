4db05d7c696ac11a29e6968c2e1cf4a6c4760259
joseignacioblanco <tombonia@gmail.com>
2024-11-15 04:28:38 -0300

    comenzando a construir el telegram bot. estable en beta

--- bot_estados.py
+++ bot_estados.py
@@ -1,9 +1,13 @@
+#! /usr/bin/python3
+
 import telebot
 import RPi.GPIO as GPIO
 
+
 # Configura los GPIO
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.OUT)  # Ejemplo: Pin 7 como salida
+GPIO.setup(7, GPIO.OUT)  # Pin 7 como salida, TODO:darle nombres y poner constanstes simbolicas
+
 
 # Token de tu bot
 #BOT_TOKEN = "6934161704:AAEG8TayLzH0VRerDJCOyf5V95Q4-USxcvg" este es el bot de iyire
@@ -14,43 +18,85 @@ BOT_TOKEN = '6739139472:AAG4gSZYEWjtjiUdyACO-eL-0u0nhB9dZHM' #este es el bot de 
 # Crea un objeto bot
 bot = telebot.TeleBot(BOT_TOKEN)
 
+estado_gpio = {7: False} # Diccionario para almacenar el estado de los GPIO TODO: iniciar todos en False
+
+
+
+
+
 # Define los comandos
-@bot.message_handler(commands=['encender'])
+
+@bot.message_handler(commands=['bloquear_puerta', '/bloquear_puerta@plumistachi_bot'])
 def encender(message):
     GPIO.output(7, GPIO.HIGH)
-    bot.reply_to(message, "Dispositivo encendido.")
+    estado_gpio[7] = True
+    bot.reply_to(message, "Puertas Bloqueadas!")
 
-@bot.message_handler(commands=['apagar'])
+
+
+@bot.message_handler(commands=['desbloquear_puerta'])
 def apagar(message):
     GPIO.output(7, GPIO.LOW)
-    bot.reply_to(message, "Dispositivo apagado.")
+    estado_gpio[7] = False
+    bot.reply_to(message, "Puertas Desbloqueadas.")
+
+
+
+#agregar aqio funcionalidades
+
+
+
+
+
+
+@bot.message_handler(commands=['estado_del_sistema'])
+def estado(message):
+    if estado_gpio[7]:
+        bot.reply_to(message, "La puerta esta Bloqueada.")
+    else:
+        bot.reply_to(message, "La Puerta esta Desbloqueada.")
+
+
+
+
+@bot.message_handler(commands=['ayuda'])
+def ayuda(message):
+    bot.reply_to(message, 'MENU DE OPCIONES: \n\n' + '🔐 /bloquear_puerta \n' + '🔓 /desbloquear_puerta \n\n' + '🦗 /con_chicharra \n' + '🔇 /sin_chicharra \n\n' + '📊 /estado_del_sistema : Estado general del sistema \n' + '🆘 /ayuda : Muestra este menu')
+
+
+
+
 
 # Ejecuta el bot
 bot.polling()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-# ... (código anterior)
-
-# Diccionario para almacenar el estado de los GPIO
-estado_gpio = {17: False}
-
-# ... (funciones encender y apagar modificadas para actualizar el estado)
-
-@bot.message_handler(commands=['estado'])
-def estado(message):
-    if estado_gpio[17]:
-        bot.reply_to(message, "El dispositivo está encendido.")
-    else:
-        bot.reply_to(message, "El dispositivo está apagado.")
+       
+        
+if __name__ == "__main__":
+    main()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    '''
+    try:
+    
+    
+    
+    except KeyboardInterrupt:
+            print("\nSaliendo del sistema.")
+            break
+        finally:
+            GPIO.cleanup()
+    
+    
+    
+    '''
