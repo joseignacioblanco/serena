77d34f1e9c21e95bfef5d4c9bd78d9c2e44e9bc7
joseignacioblanco <tombonia@gmail.com>
2024-11-15 20:20:11 -0300

    agrego reflector y alama y acomodo el main. andando.

--- bot_estados.py
+++ bot_estados.py
@@ -6,8 +6,10 @@ import RPi.GPIO as GPIO
 
 #Asociacion de pines en modo BOARD
 MAGNETIC_LOCK_PIN = 7
-BUZZER_PIN = 11 # 15, 29, 31, 33, 37
+BUZZER_PIN = 11
 LUZ_VEREDA_PIN = 13
+REFLECTOR_PIN = 15
+ALARMA_PIN = 29 # disponibles 31, 33, 37
 
 
 
@@ -42,12 +44,16 @@ GPIO.output(BUZZER_PIN, ON)
 GPIO.setup(LUZ_VEREDA_PIN, GPIO.OUT)  # Pin 13 como salida.
 GPIO.output(LUZ_VEREDA_PIN, AUTOMATIC)
 
+GPIO.setup(REFLECTOR_PIN, GPIO.OUT)  # Pin 15 como salida.
+GPIO.output(REFLECTOR_PIN, OFF)
 
+GPIO.setup(ALARMA_PIN, GPIO.OUT)  # Pin 29 como salida.
+GPIO.output(ALARMA_PIN, OFF)
 
 
 
 # Configuracion inicial del diccionario de estados:
-estado_gpio = {MAGNETIC_LOCK_PIN: BLOQUEADA, BUZZER_PIN: PRENDIDO, LUZ_VEREDA_PIN: MODO_AUTOMATICO} # Diccionario para almacenar el estado de los GPIO TODO: iniciar todos en False. por ahora uso un diccionario para saber el estado de los pines, cuando sea grande  ya voy a aprender  a leer el estado de los pines con una funcion gpio.rrid o algo asi.
+estado_gpio = {MAGNETIC_LOCK_PIN: BLOQUEADA, BUZZER_PIN: PRENDIDO, LUZ_VEREDA_PIN: MODO_AUTOMATICO, REFLECTOR_PIN: APAGADO, ALARMA_PIN: APAGADO} # Diccionario para almacenar el estado de los GPIO TODO: iniciar todos en False. por ahora uso un diccionario para saber el estado de los pines, cuando sea grande  ya voy a aprender  a leer el estado de los pines con una funcion gpio.rrid o algo asi.
 
 #----------------------------------------------------------------------------------------------------
 
@@ -105,9 +111,36 @@ def luz_vereda_manual(message):
     bot.reply_to(message, "LUZ vereda MODO MANUAL.")
 
 
+# COMANDO REFLECTOR PRENDER
+@bot.message_handler(commands=['reflector_prender', '/reflector_prender@plumistachi_bot'])
+def reflector_prender(message):
+    GPIO.output(REFLECTOR_PIN, ON)
+    estado_gpio[REFLECTOR_PIN] = PRENDIDO
+    bot.reply_to(message, "REFLECTOR PRENDIDO!")
 
 
+# COMANDO REFLECTOR APAGAR
+@bot.message_handler(commands=['reflector_apagar', '/reflector_apagar@plumistachi_bot'])
+def reflector_apagar(message):
+    GPIO.output(REFLECTOR_PIN, OFF)
+    estado_gpio[REFLECTOR_PIN] = APAGADO
+    bot.reply_to(message, "REFLECTOR APAGADO!")
 
+
+# COMANDO ALARMA PRENDER
+@bot.message_handler(commands=['alarma_prender', '/alarma_prender@plumistachi_bot'])
+def alarma_prender(message):
+    GPIO.output(ALARMA_PIN, ON)
+    estado_gpio[ALARMA_PIN] = PRENDIDO
+    bot.reply_to(message, "ALARMA ENCENDIDA!!!")
+
+
+# COMANDO ALARMA APAGAR
+@bot.message_handler(commands=['alarma_apagar', '/alarma_apagar@plumistachi_bot'])
+def alarma_apagar(message):
+    GPIO.output(ALARMA_PIN, OFF)
+    estado_gpio[ALARMA_PIN] = APAGADO
+    bot.reply_to(message, "ALARMA APAGADA.")
 
 
 
@@ -139,18 +172,44 @@ def estado_del_sistema(message):
         bot.reply_to(message, "La luz de la vereda esta en modo MANUAL.")
 
 
+    if estado_gpio[REFLECTOR_PIN]:
+        bot.reply_to(message, "El reflector esta prendido.")
+    else:
+        bot.reply_to(message, "El reflector esta apagado.")
+        
+        
+    if estado_gpio[ALARMA_PIN]:
+        bot.reply_to(message, "ALARMA PRENDIDA! SONANDO!!.")
+    else:
+        bot.reply_to(message, "ALARMA APAGADA...")
+
+
+
+
+
+
+
+
 
 # COMANDO AYUDA
 @bot.message_handler(commands=['ayuda','ayuda@plumistachi_bot'])
 def ayuda(message):
-    bot.reply_to(message, 'MENU DE OPCIONES: \n\n' + '🔐 /bloquear_puerta \n' + '🔓 /desbloquear_puerta \n\n' + '🦗 /con_chicharra \n' + '🔇 /sin_chicharra \n\n' + '🅰️ /luz_vereda_autom \n' + 'Ⓜ️ /luz_vereda_manual \n\n' + '📊 /estado_del_sistema : Estado general del sistema \n' + '🆘 /ayuda : Muestra este menu')
+    bot.reply_to(message, 'MENU DE OPCIONES: \n\n' + '🔐 /bloquear_puerta \n' + '🔓 /desbloquear_puerta \n\n' + '🦗 /con_chicharra \n' + '🔇 /sin_chicharra \n\n' + '🅰️ /luz_vereda_autom \n' + 'Ⓜ️ /luz_vereda_manual \n\n' + '☀️ /reflector_prender \n' + '🌚 /reflector_apagar \n\n' + '📢 /alarma_prender \n' + '🔕 /alarma_apagar \n\n' + '📊 /estado_del_sistema : Estado general del sistema \n' + '🆘 /ayuda : Muestra este menu')
 
 
 #-----------------------------------------------------------------------------------------------------
 
 
 # Ejecuta el bot
-bot.polling()
+def main():
+    try:
+        
+        bot.polling()
+        
+    except KeyboardInterrupt:
+        print("\nSaliendo del sistema.")
+    finally:
+        GPIO.cleanup()
 
 
 
@@ -159,26 +218,3 @@ bot.polling()
 if __name__ == "__main__":
     main()
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    '''
-    try:
-    
-    
-    
-    except KeyboardInterrupt:
-            print("\nSaliendo del sistema.")
-            break
-        finally:
-            GPIO.cleanup()
-    
-    
-    
-    '''
